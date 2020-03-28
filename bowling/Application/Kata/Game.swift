46007bcaf4ca5fd24e9d486a9f571e33f074ffe1
None <None@gmail.com>
2020-03-28 13:38:18 +0000

    Ready for next time

--- bowling/Application/Kata/Game.swift
+++ bowling/Application/Kata/Game.swift
@@ -8,89 +8,14 @@
 
 import Foundation
 
-class Frame {
-    var firstRoll: Int = 0
-    var secondRoll: Int = 0
-    var firstRollPlayed = false
-    var finished = false
-    private let kSpare = 10
-    var rollScore: Int {
-        return firstRoll + secondRoll
-    }
-    func isSpare() -> Bool {
-        if firstRoll + secondRoll == 10 {
-            return true
-        }
-        return false
-    }
-    func isStrike() -> Bool {
-        if firstRoll == 10 {
-            return true
-        }
-        return false
-    }
-
-    func roll(_ pins: Int) {
-        if !firstRollPlayed {
-            firstRoll = pins
-            firstRollPlayed = true
-        } else {
-            secondRoll = pins
-            finished = true
-        }
-    }
-}
 
 class Game {
-    private var currentScore = 0
-    private var frameIndex = 0
-    private var frames = [Frame]()
-    private var bonusFrame = Frame()
-    init() {
-        for _ in 1...11 {
-            let frame = Frame()
-            frames.append(frame)
-        }
-    }
 
     func roll(_ pins: Int) {
-        if frameIndex == 10 { // Bonus
-            bonusFrame.roll(pins)
-            return
-        }
-        let frame = frames[frameIndex]
-        frame.roll(pins)
-        frames[frameIndex] = frame
-        if frame.isStrike() {
-            frameIndex += 1
-        }
-        if frame.finished {
-            frameIndex += 1
-        }
+
     }
 
     func score() -> Int {
-        var finalScore = 0
-        var cpt = 0
-        while cpt < frames.count {
-            let frame = frames[cpt]
-            var frameScore = frame.rollScore
-            if frame.isStrike() {
-                frameScore += strikeBonus(cpt + 1)
-            } else if frame.isSpare() {
-                frameScore += spareBonus(cpt + 1)
-            }
-            finalScore += frameScore
-            cpt += 1
-        }
-        return finalScore + bonusFrame.rollScore
-    }
-
-    fileprivate func spareBonus(_ cpt: Int) -> Int {
-        return frames[cpt].firstRoll
-    }
-
-    fileprivate func strikeBonus(_ cpt: Int) -> Int {
-        return frames[cpt].rollScore
+        return 0
     }
 }
--- bowling/Application/Kata/GameTests.swift
+++ bowling/Application/Kata/GameTests.swift
@@ -15,7 +15,7 @@ class GameTests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         super.setUp()
-        game = Game()
+
     }
 
     override func tearDown() {
@@ -23,58 +23,4 @@ class GameTests: XCTestCase {
         super.tearDown()
     }
 
-    func testGutterGame() {
-        rollMany(rolls: 20, pins: 0)
-        XCTAssertTrue(game.score() == 0)
-    }
-
-    func testAllOnes() {
-        rollMany(rolls: 20, pins: 1)
-        XCTAssertEqual(game.score(), 20)
-    }
-
-    func testOneSpare() {
-        rollSpare() // spare
-        game.roll(3)
-        rollMany(rolls: 17, pins: 0)
-        XCTAssertEqual(game.score(), 16)
-    }
-
-    func testOneStrike() {
-        rollStrike()
-        game.roll(3)
-        game.roll(4)
-        rollMany(rolls: 17, pins: 0)
-        XCTAssertEqual(game.score(), 24)
-    }
-
-    func testSpareLastFrame() {
-        rollMany(rolls: 19, pins: 0)
-        rollSpare()
-        game.roll(3)
-        XCTAssertEqual(game.score(), 13)
-    }
-
-    func testStrikeLastFrame() {
-        rollMany(rolls: 19, pins: 0)
-        rollStrike()
-        game.roll(3)
-        game.roll(3)
-        XCTAssertEqual(game.score(), 16)
-    }
-
-    fileprivate func rollStrike() {
-        game.roll(10)
-    }
-
-    fileprivate func rollSpare() {
-        game.roll(5)
-        game.roll(5)
-    }
-
-    func rollMany(rolls: Int, pins: Int) {
-        for _ in 1...rolls {
-            game.roll(pins)
-        }
-    }
 }
