047b78a8222620b529d1537037cb7b3513c98a59
None <None@gmail.com>
2020-03-29 13:52:02 +0100

    let's do it one more time

--- bowling/Application/Kata/Game.swift
+++ bowling/Application/Kata/Game.swift
@@ -8,12 +8,13 @@
 import Foundation
 
 class Game {
+    var theScore = 0
 
     func roll(_ pins: Int) {
-
+        theScore += pins
     }
 
     func score() -> Int {
-        return 0
+        return theScore
     }
 }
--- bowling/Application/Kata/GameTests.swift
+++ bowling/Application/Kata/GameTests.swift
@@ -14,12 +14,27 @@ class GameTests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         super.setUp()
+        game = Game()
 
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+
+    func testGutterGame() {
+        for _ in 1...20 {
+            game.roll(0)
+        }
+        XCTAssertEqual(0, game.score())
+    }
+
+    func testAllOnes() {
+        for _ in 1...20 {
+            game.roll(1)
+        }
+        XCTAssertEqual(20, game.score())
     }
 
 }
